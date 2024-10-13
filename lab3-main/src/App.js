aa71dd10797bbf857cf4bf1be647ea784146025f
Cathal Mulhern <123952675+CathalMulhern02@users.noreply.github.com>
2024-10-13 22:00:05 +0000

    Add files via upload

---
+++ lab3-main/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import the components
+import Content from './Components/contents';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import NavigationBar from './Components/navigationbar';
+import Read from './Components/read';
+
+
+function App() {
+  return (
+    // Use Components to create the pages
+    // Use the Router component to enable navigation between different pages
+    <Router>
+      <div>
+        {/* Include the navigation bar at the top of the page */}
+        <NavigationBar />
+
+        {/* Define the different routes for the application using Routes */}
+        <Routes>
+          {/* Render the Content component */}
+          <Route path="/home" element={<Content />} />
+
+          {/* Render the Read component. This will display movies */}
+          <Route path="/read" element={<Read />} />
+
+          {/* Render the Footer component*/}
+          <Route path="/create" element={<Footer />} />
+        </Routes>
+
+        {/* Display the footer */}
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
